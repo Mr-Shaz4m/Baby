f9fb814bf9a5049ad1efee84a56bccd798c1221e
Mr-Shaz4m <91620360+Mr-Shaz4m@users.noreply.github.com>
2021-10-02 19:56:25 +0545

    Update baby.c

--- baby.c
+++ baby.c
@@ -1,16 +1,17 @@
 #include <stdio.h>
-int main(){
+/* age checker :) */
+int main()
+{
 	int a;
 	printf("\033[34,41,1mEnter your age=");
 	scanf("%d",&a);
 	if(a>12&&a<20)
-	printf("your teen");
+	printf("you are teen");
 	else if(a>20&&a<50)
-	printf("your Adult");
+	printf("you are Adult");
 	else if(a>=50)
-	printf("your Old");
-	else
-	
-		
-	printf("your baby");
-	return 0;}
+	printf("you are Old");
+	else if(a>0&&a<=12)	
+	printf("you are baby");
+	return 0;
+}
